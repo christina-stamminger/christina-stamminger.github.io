38b9482f6de87fe1e6bdf911adb3ac0bc80f6008
Christina Stamminger <christina-stamminger@gmail.com>
2024-02-04 20:05:51 +0100

    changes in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dist', // Adjust to your repository name
+  base: '/dist/myWebsite/', // Adjust to your repository name
   plugins: [react()],
   build: {
     outDir: 'dist',
